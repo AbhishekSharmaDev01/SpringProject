4b7ecbeb45cbe5b223f604dbef8a18286b11c219
Abhishek sharma <as887633@gmail.com>
2026-07-02 23:53:04 +0530

    More Transaction Logic

--- spring/src/main/java/com/practice/spring/Controller/TransactionController.java
+++ spring/src/main/java/com/practice/spring/Controller/TransactionController.java
@@ -1,13 +1,18 @@
 package com.practice.spring.Controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.practice.spring.DTO.TransactionDTO;
 import com.practice.spring.Entity.Transaction;
 import com.practice.spring.Service.TransactionService;
+
+import java.util.Collection;
 
 
 @RestController
@@ -17,7 +22,15 @@ public class TransactionController{
     TransactionService transService;
 
     @PostMapping
-    public Transaction saveTheTRans(@RequestBody Transaction rb ){
-        return transService.saveTrans(rb);
+    public Transaction addTransaction(@RequestBody TransactionDTO rb ){
+        return transService.saveTransaction(rb);
+    }
+    @GetMapping
+    public Collection<Transaction> getTransaction(){
+        return transService.getTransaction();
+    }
+    @GetMapping("/{id}")
+    public Transaction getTransaction(@RequestParam String id){
+        return transService.getTransactionId(id);
     }
 }
--- spring/src/main/java/com/practice/spring/Repository/TransactionRepo.java
+++ spring/src/main/java/com/practice/spring/Repository/TransactionRepo.java
@@ -1,19 +1,25 @@
 package com.practice.spring.Repository;
 
+import java.util.Collection;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
-
 import org.springframework.stereotype.Repository;
 
 import com.practice.spring.Entity.Transaction;
 
 @Repository
 public class TransactionRepo{
-private final Map<String,Transaction> db = new ConcurrentHashMap<>();
+    private final Map<String,Transaction> db = new ConcurrentHashMap<>();
 
-public Transaction save(Transaction transaction){
-    db.put(transaction.getTransactionId(),transaction);
-    return transaction;
-}
+    public Transaction save(Transaction transaction){
+        db.put(transaction.getTransactionId(),transaction);
+        return transaction;
+    }
+    public Collection<Transaction> getTransaction(){
+        return db.values();
+    }
+    public Transaction getTransactionById(String id){
+        return db.get(id);
+    }
 
 }
